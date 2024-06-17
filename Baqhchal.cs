fcc84ade17130b92435706911d763b1c20045459
konstantin pasic <konstantin.pasic@gmail.com>
2024-06-17 11:56:32 +0200

    Added baqhchal class

---
+++ Baqhchal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baqhchal
+{
+    public class Baqhchal
+    {
+        private int tableSize = 5;
+        private int[,] board;
+        private int numSheep;
+        private bool sheepTurn;
+
+        Baqhchal(int tableSize)
+        {
+            this.tableSize = tableSize;
+            board = new int[tableSize, tableSize];
+            numSheep = 0;
+            sheepTurn = true;
+        }
+
+        public void MakeMove(int startIndex, int endIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
