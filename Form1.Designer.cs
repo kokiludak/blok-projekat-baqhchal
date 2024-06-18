d55e5d840b169745e2b6e5531fc4d05245dc9398
ciganisijusmaximius <vlajko.stankovic07@gmail.com>
2024-06-18 15:46:07 +0200

    begin form1 implementation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playerIsSheep = new System.Windows.Forms.CheckBox();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.textUserDepth = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // playerIsSheep
+            // 
+            this.playerIsSheep.AutoSize = true;
+            this.playerIsSheep.Location = new System.Drawing.Point(692, 38);
+            this.playerIsSheep.Name = "playerIsSheep";
+            this.playerIsSheep.Size = new System.Drawing.Size(80, 17);
+            this.playerIsSheep.TabIndex = 0;
+            this.playerIsSheep.Text = "100% rights";
+            this.playerIsSheep.UseVisualStyleBackColor = false;
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(692, 101);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 1;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // textUserDepth
+            // 
+            this.textUserDepth.Location = new System.Drawing.Point(688, 154);
+            this.textUserDepth.Name = "textUserDepth";
+            this.textUserDepth.Size = new System.Drawing.Size(100, 20);
+            this.textUserDepth.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.textUserDepth);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.playerIsSheep);
+            this.DoubleBuffered = true;
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox playerIsSheep;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.TextBox textUserDepth;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,24 @@ namespace Baqhchal
 {
     public partial class Form1 : Form
     {
+        const int tableSize = 5;
+        
+        Baqhchal tabla;
+        Engine protivnik;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabla = new Baqhchal(tableSize);
+            protivnik = new Engine(Int32.Parse(textUserDepth.Text), tabla, playerIsSheep.Checked);
+        }
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
         }
     }
 }
